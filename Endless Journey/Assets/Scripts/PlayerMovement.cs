c6432fa529d053d4e2a2970c938664071c884ffc
zlobindenis35@gmail.com <zlobindenis35@gmail.com>
2023-05-24 21:28:25 +0500

    Анимацию бега, поворот, организация файлов

--- Endless Journey/Assets/Scripts/PlayerMovement.cs
+++ Endless Journey/Assets/Scripts/PlayerMovement.cs
@@ -1,27 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jump;
+    private Animator anim;
+    private SpriteRenderer sprite;
     private Rigidbody2D body;
 
+    private float coordX = 0f;
     public bool isJumping;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed, body.velocity.y);
+
+        coordX = Input.GetAxisRaw("Horizontal");
+
+        body.velocity = new Vector2(coordX*speed, body.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space) && isJumping == false)
             body.AddForce(new Vector2(body.velocity.x, jump));
+
+        UpdateAnimation();
     }
 
+    private void UpdateAnimation()
+    {
+        if (coordX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (coordX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {

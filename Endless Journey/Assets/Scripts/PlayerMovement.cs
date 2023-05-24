3433f4181e02b9dee2c93d686d94f5311775bf7b
MonoLLL <61151102+MonoLLL@users.noreply.github.com>
2023-05-24 18:31:57 +0500

    Фикс пыржка

--- Endless Journey/Assets/Scripts/PlayerMovement.cs
+++ Endless Journey/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jump;
     private Rigidbody2D body;
+
+    public bool isJumping;
 
     private void Awake()
     {
@@ -15,7 +18,24 @@ public class PlayerMovement : MonoBehaviour
     {
         body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed, body.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKeyDown(KeyCode.Space) && isJumping == false)
+            body.AddForce(new Vector2(body.velocity.x, jump));
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isJumping = true;
+        }
     }
 }

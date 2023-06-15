b359ae331440fff3132e23011cd9602f4a6523e8
zlobindenis35@gmail.com <zlobindenis35@gmail.com>
2023-06-15 18:16:28 +0500

    фикс смерти врагов

--- Endless Journey/Assets/Scripts/EnemyHealth.cs
+++ Endless Journey/Assets/Scripts/EnemyHealth.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
@@ -20,7 +21,14 @@ public class EnemyHealth : MonoBehaviour
         {
             SoundManager.instance.PlaySound(deathSound,  SoundManager.currentVolume);
             anim.SetTrigger("death");    //Здесь должна быть анимация смерти
-            //GetComponent<PlayerMovement>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<PatrolScript>().enabled = false;
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            Invoke(nameof(Death), 0.7f);
         }
+    }
+    public void Death()
+    {
+        Destroy(gameObject);
     }
 }
